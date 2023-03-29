2351deb312aa53eefe5b0b1dfdaee5bf3cba5e7b
Paweł Jabłonowski <pajablonowski@gmail.com>
2023-03-29 13:55:41 +0200

    connecting controller with repository

--- src/main/java/com/amigoscode/controller/CustomerController.java
+++ src/main/java/com/amigoscode/controller/CustomerController.java
@@ -2,17 +2,27 @@ package com.amigoscode.controller;
 
 
 import com.amigoscode.model.Customer;
+import com.amigoscode.repository.CustomerRepository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("api/vq/customers")
 public class CustomerController {
-    @GetMapping("/users")
-    public List<Customer> getuserList(){
-        return List.of(
-        );
+
+    private final CustomerRepository customerRepository;
+
+    public CustomerController(CustomerRepository customerRepository){
+        this.customerRepository = customerRepository;
+    }
+
+
+    @GetMapping
+    public List<Customer> getCustomersList(){
+        return customerRepository.findAll();
     }
 
 }
