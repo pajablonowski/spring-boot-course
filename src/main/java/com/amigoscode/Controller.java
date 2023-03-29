2a349124b3bd19410c08113aed4bd534337a960b
Paweł Jabłonowski <pajablonowski@gmail.com>
2023-03-29 09:47:28 +0200

    Parsing objects to JSON

--- src/main/java/com/amigoscode/Controller.java
+++ src/main/java/com/amigoscode/Controller.java
@@ -9,7 +9,37 @@ import java.util.List;
 public class Controller {
 
     @GetMapping("/greetings")
-    public List<String> greetings() {
-        return List.of("Hi","Hello","Buongiorno");
+    public List<GreetResponse> greetings() {
+        return List.of(
+                new GreetResponse(
+                        1,
+                        "Hi",
+                        List.of("AA", "BB"),
+                        new Person(
+                                "Paweł",
+                                34,
+                                true,
+                                30_000)),
+                new GreetResponse(
+                        2,
+                        "Hello",
+                        List.of("CC", "DD"),
+                        new Person(
+                                "Aga",
+                                30,
+                                false,
+                                45_00.76))
+        );
+    }
+
+    record Person(String firstName, int age, boolean active, double savings) {
+    }
+
+    record GreetResponse(
+            Integer id,
+            String greet,
+            List<String> favProgrammingLanguage,
+            Person person
+    ) {
     }
 }
